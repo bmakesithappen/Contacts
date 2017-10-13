322027a4d186a6d0e98220b38ff248f52a5dd7b4
Bernard Desert <bdesert@etsy.com>
2017-10-13 16:14:40 -0400

    First Commit

---
+++ Contacts/Contacts.swift
@@ -0,0 +1,15 @@
+//
+//  Contacts.swift
+//  Contacts
+//
+//  Created by Bernard Désert on 10/13/17.
+//  Copyright © 2017 Bernard Désert. All rights reserved.
+//
+
+import UIKit
+
+class Contacts: NSObject {
+    var name: String?
+    var phoneNumber: String?
+
+}
---
+++ Contacts/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Bernard Désert on 10/13/17.
+//  Copyright © 2017 Bernard Désert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+ /*   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "openContactInfo") {
+            let newViewController = segue.destination
+        
+    }
+
+*/
+}
+
