4f853ae7b9dd0ed47b84daa35a6fbbbd25037495
Bernard Desert <bdesert@etsy.com>
2017-10-16 17:50:31 -0400

    Updated comments

--- Contacts/Contacts.swift
+++ Contacts/Contacts.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+// TODO: Condense comments line 13 = 19
 
-class Contacts: NSObject {
+/*
+  This file is a Contact object to represent each contact and their data.
+ 
+ Because both properties are optional, we aren't required to create an initializer function. For the sake of convenience (and practice!) though, let's add one anyway:
+ Because we made the properties of the Contact class optional and we gave them both a default value of nil in the initializer, we can create a Contact object no matter which properties we know at the time of creation. All three of these statements are valid:
+ 
+ The properties not specified at init time will just default to nil. This gives us the flexibility of using init with just the data we have but retains the safety of optionals.
+ */
+
+class Contacts: NSObject { // BD: Swift objects are not required to inherit do this if 1. if are you accessing old features(NSObject) of OBjC and makes it more readable for yourself and others
+    
     var name: String?
     var phoneNumber: String?
     
+    // instantiate variables ,
     init(name: String? = nil, phoneNumber: String? = nil) {
         self.name = name
         self.phoneNumber = phoneNumber
@@ -19,3 +31,4 @@ class Contacts: NSObject {
     }
 
 }
+
--- Contacts/ContactsTableViewController.swift
+++ Contacts/ContactsTableViewController.swift
@@ -12,10 +12,10 @@ class ContactsTableViewController: UITableViewController {
     
     var contacts:[Contacts] = []
     
-    func toggleEdit() {
+    @objc func toggleEdit() { // BD: @objc because this method has not been updated for Swift for the moveButton line 29 remvoe @obj see error review this again to update this comment
         tableView.setEditing(!tableView.isEditing, animated: true)
-    }
-
+    };
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,41 +26,41 @@ class ContactsTableViewController: UITableViewController {
         self.contacts.append(jenny)
         self.contacts.append(rich)
         self.contacts.append(mindy)
-
- 
+        
         let moveButton = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(ContactsTableViewController.toggleEdit))
         navigationItem.leftBarButtonItem = moveButton
         
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controlle r.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1 // BD: Changed this
+        return 1 // BD: return value is the number of sections we want displayed
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return self.contacts.count // BD: Changed this
+        return self.contacts.count // BD: return value is the number of rows we want displayed which is equal to how many (.count) of contacts we have this can vary depending on what the count is
     }
-
-
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)  // BD: What did I change here
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    
+        // BD: "cell" arbitrary name that we choose to help identify what data is populating cells:cell type
         
+        // Configure the cell...
         let contact = self.contacts[indexPath.row]
         
         if let name = contact.name {
@@ -71,16 +71,16 @@ class ContactsTableViewController: UITableViewController {
         
         return cell
     }
-
-
+    
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
-
-
+    
+    
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -91,28 +91,28 @@ class ContactsTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-
-
+    
+    
     
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
         let contactMoving = contacts.remove(at: fromIndexPath.row)
         contacts.insert(contactMoving, at: to.row)
-
+        
     }
-
-
+    
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let indexPath = self.tableView.indexPath(for: sender as! UITableViewCell)!
@@ -120,9 +120,9 @@ class ContactsTableViewController: UITableViewController {
         let destination = segue.destination as! DetailsViewController
         destination.contact = contact
     }
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     
-
-
+    
+    
 }
